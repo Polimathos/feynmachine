b3e13587778a51583b5ed91b2a2f3a7bb34bd78c
Marcos Gonzalo Lopez <mgonzalo@stratio.com>
2018-12-10 09:44:06 +0100

    Ogma pipeline working completelly

--- src/main/scala/body/Main.scala
+++ src/main/scala/body/Main.scala
@@ -3,7 +3,6 @@ package body
 import org.apache.spark._
 import org.apache.spark.streaming._
 import com.ogmacorp.ogmaneo._
-
 import java.io._
 
 object Main extends App {
@@ -24,6 +23,7 @@ object Main extends App {
   ssc.awaitTermination()*/
 
   val numSimSteps = 100
+
   val res = new Resources()
   res.create(ComputeSystem.DeviceType._gpu)
 
@@ -37,41 +37,22 @@ object Main extends App {
   inputParams.setValue("in_p_alpha", 0.02f)
   inputParams.setValue("in_p_radius", 8)
 
-  var i = 0
-  while ( {
-    i < 2
-  }) {
+  for (i <- 0 to 2){
     val layerParams = arch.addHigherLayer(new Vec2i(32, 32), SparseFeaturesType._chunk)
     layerParams.setValue("sfc_numSamples", 2)
-
-    {
-      i += 1; i - 1
-    }
   }
 
   val hierarchy = arch.generateHierarchy
 
   val inputField = new ValueField2D(new Vec2i(w, h))
 
-  var y = 0
-  while ( {
-    y < h
-  }) {
-    var x = 0
-    while ( {
-      x < w
-    }) {
-      inputField.setValue(new Vec2i(x, y), (y * w) + x)
-
-      {
-        x += 1
-      }
-    }
-
-    {
-      y += 1
-    }
+  for {
+    y <- 0 to h
+    x <- 0 to w
+  } {
+    inputField.setValue(new Vec2i(x, y), (y * w) + x)
   }
+
   private val serializationEnabled = false
 
   if (serializationEnabled) {
@@ -83,67 +64,38 @@ object Main extends App {
   }
 
   System.out.println("Stepping the hierarchy...")
-  var i2 = 0
-  while ( {
-    i2 < numSimSteps
-  }) {
+
+  for (i <- 0 to numSimSteps) {
     val inputVector = new vectorvf
     inputVector.add(inputField)
     hierarchy.activate(inputVector)
     hierarchy.learn(inputVector)
     //System.out.print(".");
-
-    {
-      i += 1
-    }
   }
+
   System.out.println()
 
   val prediction = hierarchy.getPredictions.get(0)
 
-  while (true) {
-    System.out.print("Input      :")
-    var ye = 0
-    while ( {
-      ye < h
-    }) {
-      var x = 0
-      while ( {
-        x < w
-      }) {
-        System.out.printf(" %.2f", inputField.getValue(new Vec2i(x, ye)).toString)
-
-        {
-          x += 1
-        }
-      }
-
-      {
-        ye += 1
-      }
+  for(i <- 0 to numSimSteps) {
+    System.out.println(i)
+    for {
+      y <- 0 to h
+      x <- 0 to w
+    } {
+      System.out.print(inputField.getValue(new Vec2i(x, y)).toString+" ")
     }
     System.out.println()
+
     System.out.print("Prediction :")
-    var y = 0
-    while ( {
-      y < h
-    }) {
-      var x = 0
-      while ( {
-        x < w
-      }) {
-        System.out.printf(" %.2f", prediction.getValue(new Vec2i(x, y)).toString)
-
-        {
-          x += 1; x - 1
-        }
-      }
-
-      {
-        y += 1; y - 1
-      }
+    for {
+      y <- 0 to h
+      x <- 0 to w
+    } {
+      System.out.print(prediction.getValue(new Vec2i(x, y)).toString+" ")
     }
     System.out.println()
+    System.out.println(i)
     if (serializationEnabled) {
       System.out.println("Saving hierarchy to Example.opr")
       hierarchy.save(res.getComputeSystem, "Example.opr")
