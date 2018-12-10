ca4b156da525b74ccd568e36530017ba42600fc0
Marcos Gonzalo Lopez <mgonzalo@stratio.com>
2018-12-10 08:18:28 +0100

    library fixed

---
+++ lib/build/CMakeFiles/JOgmaNeo.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "src/main/java/com/ogmacorp/ogmaneo/jogmaneoJAVA_wrap.cxx"
+  "src/main/java/com/ogmacorp/ogmaneo/jogmaneo.java"
+  "src/main/java/com/ogmacorp/ogmaneo/jogmaneoJNI.java"
+  "src/main/java/com/ogmacorp/ogmaneo/jogmaneoJAVA_wrap.cxx"
+  "src/main/java/com/ogmacorp/ogmaneo/jogmaneo.java"
+  "src/main/java/com/ogmacorp/ogmaneo/jogmaneoJNI.java"
+  "CMakeFiles/JOgmaNeo.dir/src/main/java/com/ogmacorp/ogmaneo/jogmaneoJAVA_wrap.cxx.o"
+  "libJOgmaNeo.pdb"
+  "libJOgmaNeo.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JOgmaNeo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
